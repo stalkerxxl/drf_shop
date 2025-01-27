cd43b8ee995ec46031fcc6bd7e70ebf629471564
Roman A. <stalkerxxl@gmail.com>
2025-01-27 10:51:41 +0400

    rewrite test_views.py with permissions test

--- tests/views/conftest.py
+++ tests/views/conftest.py
@@ -5,3 +5,17 @@ from rest_framework.test import APIClient
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def admin_user(django_user_model):
+    return django_user_model.objects.create_superuser(
+        username="admin", password="admin123", email="admin@example.com"
+    )
+
+
+@pytest.fixture
+def normal_user(django_user_model):
+    return django_user_model.objects.create_user(
+        username="user", password="user123", email="user@example.com"
+    )
--- tests/views/test_views.py
+++ tests/views/test_views.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from rest_framework import status
 
 from shop_api.models import Category
 from tests.views.conftest import api_client
@@ -14,33 +15,60 @@ class TestCategoryViewSet:
     def test_category_list_view(self, api_client, test_category):
         url = reverse("category-list")
         response = api_client.get(url)
-        assert response.status_code == 200
+        assert response.status_code == status.HTTP_200_OK
         assert len(response.data) == 1
         assert response.data[0]["name"] == "Test Category"
-
-    def test_category_create_view(self, api_client):
-        url = reverse("category-list")
-        data = {"name": "New Category"}
-        response = api_client.post(url, data, format="json")
-        assert response.status_code == 201
-        assert Category.objects.filter(name="New Category").exists()
 
     def test_category_detail_view(self, api_client, test_category):
         url = reverse("category-detail", args=[test_category.id])
         response = api_client.get(url)
-        assert response.status_code == 200
+        assert response.status_code == status.HTTP_200_OK
         assert response.data["name"] == "Test Category"
 
-    def test_category_update_view(self, api_client, test_category):
+    def test_category_create_view_as_admin(self, api_client, admin_user):
+        api_client.force_authenticate(user=admin_user)
+        url = reverse("category-list")
+        data = {"name": "New Category"}
+        response = api_client.post(url, data, format="json")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Category.objects.filter(name="New Category").exists()
+
+    def test_category_create_view_as_normal_user(self, api_client, normal_user):
+        api_client.force_authenticate(user=normal_user)
+        url = reverse("category-list")
+        data = {"name": "New Category"}
+        response = api_client.post(url, data, format="json")
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_category_update_view_as_admin(self, api_client, admin_user, test_category):
+        api_client.force_authenticate(user=admin_user)
         url = reverse("category-detail", args=[test_category.id])
         data = {"name": "Updated Category"}
         response = api_client.put(url, data, format="json")
-        assert response.status_code == 200
+        assert response.status_code == status.HTTP_200_OK
         test_category.refresh_from_db()
         assert test_category.name == "Updated Category"
 
-    def test_category_delete_view(self, api_client, test_category):
+    def test_category_update_view_as_normal_user(
+            self, api_client, normal_user, test_category
+    ):
+        api_client.force_authenticate(user=normal_user)
+        url = reverse("category-detail", args=[test_category.id])
+        data = {"name": "Updated Category"}
+        response = api_client.put(url, data, format="json")
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_category_delete_view_as_admin(self, api_client, admin_user, test_category):
+        api_client.force_authenticate(user=admin_user)
         url = reverse("category-detail", args=[test_category.id])
         response = api_client.delete(url)
-        assert response.status_code == 204
+        assert response.status_code == status.HTTP_204_NO_CONTENT
         assert not Category.objects.filter(name="Test Category").exists()
+
+    def test_category_delete_view_as_normal_user(
+            self, api_client, normal_user, test_category
+    ):
+        api_client.force_authenticate(user=normal_user)
+        url = reverse("category-detail", args=[test_category.id])
+        response = api_client.delete(url)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
