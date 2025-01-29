deaa63a6482273654336365393b47c694235e4f5
Roman A. <stalkerxxl@gmail.com>
2025-01-29 13:36:59 +0400

    add product_count logic (is_staff?) to Tag serializer

---
+++ shop_api/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from shop_api.models import Category, Product, Tag
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    product_count = serializers.SerializerMethodField()
+
+    # noinspection PyUnresolvedReferences
+    class Meta:
+        model = Category
+        fields = ["url", "id", "name", "created_at", "updated_at", "product_count"]
+        extra_kwargs = {"url": {"view_name": "category-detail", "lookup_field": "pk"}}
+
+    @staticmethod
+    def get_product_count(obj):
+        return Product.objects.filter(category=obj).count()
+
+
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+    product_count = serializers.SerializerMethodField()
+
+    # noinspection PyUnresolvedReferences
+    class Meta:
+        model = Tag
+        fields = ["url", "id", "name", "created_at", "updated_at", "product_count"]
+        extra_kwargs = {"url": {"view_name": "tag-detail", "lookup_field": "pk"}}
+
+    def get_product_count(self, obj: Tag):
+        request = self.context.get("request")
+        if request and request.user.is_staff:
+            return obj.product_set.count()
+        return obj.product_set.filter(is_active=True).count()
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = "__all__"
